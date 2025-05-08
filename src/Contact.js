718e0937ce4c75b7298e71c7a54942730954531e
CaroCodingK <130165356+carocodingk@users.noreply.github.com>
2025-05-08 01:07:37 -0400

    Moved name and profession information to be fetch from aboutme.json file as well as URL for the Google form used to make contact

--- src/Contact.js
+++ src/Contact.js
@@ -1,11 +1,12 @@
 import React from "react";
+import aboutme from "./content/Aboutme.json"
 
 import "./Contact.css";
 
 function Contact(){
   return(
     <div id="contactDiv">
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScNrmhV8YnHHAJxZEc8oW651EVANF5UEsSmrlf7nRlNwYPhKA/viewform?embedded=true" width="640" height="720" frameborder="0" marginheight="0" marginwidth="0">
+      <iframe src={aboutme.contact_form} width="640" height="720" frameborder="0" marginheight="0" marginwidth="0">
         Loading…
       </iframe>        
     </div>
--- src/Introduction.js
+++ src/Introduction.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Introduction.css";
+import aboutme from "./content/Aboutme.json"
 
 function Introduction({image}){
   return(
     <div id="introDiv">
       <img id="introImage" src={image}></img>
       <div id="introText">
-        <p id="introName">Caro Coding</p>
-        <p id="introProfession">Software Engineer</p>
+        <p id="introName">{aboutme.name}</p>
+        <p id="introProfession">{aboutme.profession}</p>
       </div>
     </div>
   );
